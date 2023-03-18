624aa9cd72ffb240da6b91bc3b43c49ca0d7f82f
Nisha Yadav <vatsnishaa@gmail.com>
2023-03-18 16:39:33 +0530

    Add files via upload

---
+++ DOM - Assignment/EventListenerProject/mouseout/script4.js
@@ -0,0 +1,7 @@
+const boxElement = document.getElementById("box");
+
+boxElement.addEventListener("mouseout", function () {
+  alert(
+    "you left the safe zone 🙅‍♀️ (mouse out event is working properly)"
+  );
+});
