0a745a2fa55e481c166e8bdbecef90e04ce7dd2a
Nisha Yadav <vatsnishaa@gmail.com>
2023-03-18 16:38:04 +0530

    Add files via upload

---
+++ DOM - Assignment/EventListenerProject/mouseover/script3.js
@@ -0,0 +1,7 @@
+const boxElement = document.getElementById("box");
+
+boxElement.addEventListener("mouseover", function () {
+  alert(
+    "you entered in Restricted Area 🙅‍♀️ (mouse over event is working properly)"
+  );
+});
