05ac8b6241b85fea9f6669484ac76e46e9739996
Nisha Yadav <vatsnishaa@gmail.com>
2023-03-21 20:41:16 +0530

    Create script.js

---
+++ MileStone_Assignment/passwordValidator/script.js
@@ -0,0 +1,10 @@
+// Password Validator
+let password = "01234abc";
+let confirmpassword = "01234abc";
+
+if(password == confirmpassword) {
+    console.log("Password Matched. Password validation Successful.");
+}
+else {
+    console.log("Password didn't match. Password validation unsuccessful");
+}
