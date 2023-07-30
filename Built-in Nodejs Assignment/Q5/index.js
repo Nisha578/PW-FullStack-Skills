1e69d4e32ad92f08d4cd4902f51cc90c527cd7a6
Nisha Yadav <vatsnishaa@gmail.com>
2023-07-30 20:37:42 +0530

    Update index.js

--- Built-in Nodejs Assignment/Q5/index.js
+++ Built-in Nodejs Assignment/Q5/index.js
@@ -3,3 +3,11 @@ const fs = require('fs');
 fs.unlink("./nodejs_architecture.txt", () => {
     console.log("File Deleted Successfully");
 })
+
+
+Output:
+
+/*
+PS E:\Built-inModulesNodejsAssignment> node index.js
+File Deleted Successfully
+*/
