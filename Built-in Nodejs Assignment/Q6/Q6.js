73f76f39f0c8d8ae2fdd2afef616f2aa8d124826
Nisha Yadav <vatsnishaa@gmail.com>
2023-07-30 20:42:19 +0530

    Update Q6.js

--- Built-in Nodejs Assignment/Q6/Q6.js
+++ Built-in Nodejs Assignment/Q6/Q6.js
@@ -2,3 +2,8 @@ const os = require('os');
 
 console.log("operating system name: " +os.type());
 console.log("operating system release: "+os.release());
+
+// output:
+// PS E:\Built-inModulesNodejsAssignment> node "e:\Built-inModulesNodejsAssignment\Q6.js"
+// operating system name: Windows_NT
+// operating system release: 10.0.19045   
