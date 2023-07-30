61322e9347a886fdbbc6fe294258629c030ea919
Nisha Yadav <vatsnishaa@gmail.com>
2023-07-30 20:22:23 +0530

    Update index.js

--- Built-in Nodejs Assignment/Q3/index.js
+++ Built-in Nodejs Assignment/Q3/index.js
@@ -9,3 +9,9 @@ fs.writeFileSync("nodejs_architecture.txt", data);
 const readData = fs.readFileSync("nodejs_architecture.txt");
 
 console.log(readData.toString());
+
+
+output:
+PS E:\Built-inModulesNodejsAssignment> node index.js
+The NodeJS runs event-driven operations, where each task is triggered by the use of events which deploys a callback function. 
+The NodeJS runtime is an asynchronous operation and doesn't block other tasks while enabling a smooth operation.
