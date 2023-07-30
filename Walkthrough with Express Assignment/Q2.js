578a6c19e07e254c930f6d38fc48d4f5d386e52e
Nisha Yadav <vatsnishaa@gmail.com>
2023-07-31 01:35:58 +0530

    Add files via upload

---
+++ Walkthrough with Express Assignment/Q2.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+let counter = 0;
+
+app.get('/', (req,res) => {
+    res.json({counter: counter});
+});
+
+app.get('/increment', (req,res) => {
+    counter++;
+    res.json({counter: counter});
+});
+
+app.get('/decrement', (req, res) => {
+    counter--;
+    res.json({counter: counter});
+});
+
+const port = 3303;
+
+app.listen(port, () => {
+    console.log(`The server is running on http://localhost:${port}`);
+})
+
+
