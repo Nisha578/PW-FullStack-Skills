54c8933012a7283c394ac06eab47c6f3545c6c34
Nisha Yadav <vatsnishaa@gmail.com>
2023-07-31 01:51:25 +0530

    Update Q2.js

--- Walkthrough with Express Assignment/Q2.js
+++ Walkthrough with Express Assignment/Q2.js
@@ -1,3 +1,9 @@
+// Q.2 Create a Backend For Counter Web App with Api Endpoint for Managing Counter Using Expresl
+// " ‘/’G →G send counter data as {counter:counter^
+// " `/increment` → increment counter by 1 and send in response :atest data as {counter:counter^
+// " `/decrement` → decrement counter by 1 and send in response :atest data as {counter:counter}
+// No#e: Using Express is Mandatory for this Question
+
 const express = require('express');
 const app = express();
 
